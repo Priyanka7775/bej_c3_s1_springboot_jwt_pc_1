c9a4ad18916d534d8a2718020c1d4f7cb0ec3684
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-11-11 23:20:41 +0530

    completed

---
+++ customerPc1/src/test/java/com/example/customerPc1/CustomerPc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customerPc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerPc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
